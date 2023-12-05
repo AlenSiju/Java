05cb9e2cb952b7091fb590abb053d8041271d8b0
AlenSiju <145443262+AlenSiju@users.noreply.github.com>
2023-12-05 11:11:04 +0530

    Add files via upload

---
+++ Hellojava.java
@@ -0,0 +1,16 @@
+/*
+ * File        : Hellojava.java
+ * Description : My First Java Program
+                 "Hello Java"
+ * Author      : Alen Siju
+ * Version     : 1.0
+ * Date        : 19/09/23
+ */
+
+public class Hellojava {
+	public static void main(String [] args) {
+	    System.out.println("Hello Java");
+	    System.out.print("hello World");
+	}
+
+}
