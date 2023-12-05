fa2b27b49cb08ae2002eeeef3a12a672bfbf15e5
AlenSiju <145443262+AlenSiju@users.noreply.github.com>
2023-12-05 11:08:56 +0530

    Add files via upload

---
+++ difftwonos.java
@@ -0,0 +1,16 @@
+/*
+ * File        : difftwonos.java
+ * Description :Multiplication of two numbers
+             
+ * Author      : Alen Siju
+ * Version     : 1.0
+ * Date        : 19/09/23
+ */
+
+public class difftwonos {
+	public static void main(String [] args) {
+    	int number1=20,number2=10;
+    	int diff=number1-number2;
+    	System.out.print(diff);
+    }
+}
---
+++ divtwonos.java
@@ -0,0 +1,16 @@
+/*
+ * File        : divtwonos.java
+ * Description :Division of two numbers
+             
+ * Author      : Alen Siju
+ * Version     : 1.0
+ * Date        : 19/09/23
+ */
+public class divtwonos {
+	public static void main(String [] args) {
+    	int number1=20,number2=10;
+    	int div=number1/number2;
+    	System.out.print(div);
+    }
+}
+
